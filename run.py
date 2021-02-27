04e611aa56b612e1a510242f8ebf40a8783775e6
tchikaModz <66157532+tchikaModz@users.noreply.github.com>
2021-02-27 06:17:48 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 import os
 os.system("clear")
 print("""
- ____  ____                 _   _   _             _
-|  _ \|  _ \  ___  ___     / \ | |_| |_ __ _  ___| | __
-| | | | | | |/ _ \/ __|   / _ \| __| __/ _` |/ __| |/ /
-| |_| | |_| | (_) \__ \  / ___ \ |_| || (_| | (__|   <
-|____/|____/ \___/|___/ /_/   \_\__|\__\__,_|\___|_|\_\
+ _       _     _ _                             _
+| |_ ___| |__ (_) | ____ _ _ __ ___   ___   __| |____
+| __/ __| '_ \| | |/ / _` | '_ ` _ \ / _ \ / _` |_  /
+| || (__| | | | |   < (_| | | | | | | (_) | (_| |/ /
+ \__\___|_| |_|_|_|\_\__,_|_| |_| |_|\___/ \__,_/___|
 """)  
 os.system("chmod +x xerxes.c")
 os.system("gcc xerxes.c -o xerxes")
-a = input("\n Enter website address eg : www.fakesite.com \n Website Name : ")
+a = input("\n Entre un site : www.fakesite.com \n Website Name : ")
 os.system("./xerxes "+a+" 80")
